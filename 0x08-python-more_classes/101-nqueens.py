ba987a5eeaf22d592dd2ae370f57d834dce023c1
sirmos <sirmos>
2024-01-04 13:30:10 -0800

    Python more classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,50 +3,50 @@ import sys
 
 
 def init_board(n):
-        """Initialize an `n`x`n` sized chessboard with 0's."""
-            board = []
-                [board.append([]) for i in range(n)]
-                    [row.append(' ') for i in range(n) for row in board]
-                        return (board)
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
 
 
-                    def board_deepcopy(board):
-                            """Return a deepcopy of a chessboard."""
-                                if isinstance(board, list):
-                                            return list(map(board_deepcopy, board))
-                                            return (board)
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
 
 
-                                        def get_solution(board):
-                                                """Return the list of lists representation of a solved chessboard."""
-                                                    solution = []
-                                                        for r in range(len(board)):
-                                                                    for c in range(len(board)):
-                                                                                    if board[r][c] == "Q":
-                                                                                                        solution.append([r, c])
-                                                                                                                        break
-                                                                                                                        return (solution)
+def get_solution(board):
+    """Return the list of lists representation of a solved chessboard."""
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+            return (solution)
 
 
-                                                                                                                    def xout(board, row, col):
-                                                                                                                            # X out all forward spots
-                                                                                                                                for c in range(col + 1, len(board)):
-                                                                                                                                            board[row][c] = "x"
-                                                                                                                                                # X out all backwards spots
-                                                                                                                                                    for c in range(col - 1, -1, -1):
-                                                                                                                                                                board[row][c] = "x"
-                                                                                                                                                                    # X out all spots below
-                                                                                                                                                                        for r in range(row + 1, len(board)):
-                                                                                                                                                                                    board[r][col] = "x"
-                                                                                                                                                                                        # X out all spots above
-                                                                                                                                                                                            for r in range(row - 1, -1, -1):
-                                                                                                                                                                                                        board[r][col] = "x"
-                                                                                                                                                                                                            # X out all spots diagonally down to the right
-                                                                                                                                                                                                                c = col + 1
-                                                                                                                                                                                                                    for r in range(row + 1, len(board)):
-                                                                                                                                                                                                                                if c >= len(board):
-                                                                                                                                                                                                                                                break
-                                                                                                                                                                                                                                                    board[r][c] = "x"
+        def xout(board, row, col):
+            # X out all forward spots
+            for c in range(col + 1, len(board)):
+                board[row][c] = "x"
+                # X out all backwards spots
+                for c in range(col - 1, -1, -1):
+                    board[row][c] = "x"
+                    # X out all spots below
+                    for r in range(row + 1, len(board)):
+                        board[r][col] = "x"
+                        # X out all spots above
+                        for r in range(row - 1, -1, -1):
+                            board[r][col] = "x"
+                            # X out all spots diagonally down to the right
+                            c = col + 1
+                            for r in range(row + 1, len(board)):
+                                if c >= len(board):
+                                    break
+                                board[r][c] = "x"
                                                                                                                                                                                                                                                             c += 1
                                                                                                                                                                                                                                                                 # X out all spots diagonally up to the left
                                                                                                                                                                                                                                                                     c = col - 1
