4cab6e015ae157e00ee314da4163d2f743a1d0bd
sirmos <sirmos>
2024-01-08 07:46:16 -0800

    Python inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -19,5 +19,5 @@ class Square(Rectangle):
         self.__size = size
         super().__init__(self.__size, self.__size)
 
-        def __str__(self):
-            return("[Square] " + str(self.__size) + "/" + str(self.__size))
+    def __str__(self):
+        return("[Square] " + str(self.__size) + "/" + str(self.__size))
